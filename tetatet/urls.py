6f032f39ea9088f3b8c1eeac02759d80ec0f559c
DDreydyy <66523537+NeeMuz@users.noreply.github.com>
2026-05-25 12:39:06 +0200

    upate

--- tetatet/urls.py
+++ tetatet/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 
     path('graphql/', GraphQLView.as_view(graphiql=True), name='graphql'),
 
+    path('forgot-password/', TemplateView.as_view(template_name='forgot_password.html'), name='forgot_password'),
+
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),

eca5638270c13e6752d45692e2c2a6dbe4a92774
DDreydyy <66523537+NeeMuz@users.noreply.github.com>
2026-05-25 13:08:29 +0200

    upate

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from .views import login_view, register_view, logout_view, order_page
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('order/', order_page, name='order'),
